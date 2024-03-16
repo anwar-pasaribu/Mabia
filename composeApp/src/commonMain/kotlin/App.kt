07414af6b0886452e51b1905e1300955b446cfec
anwar <anwar_pasaribu@live.com>
2024-03-17 00:52:34 +0700

    Integrate PreCompose as Navigation ROuting

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,13 +1,16 @@
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.fadeIn
+import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.togetherWith
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import moe.tlaster.precompose.PreComposeApp
+import moe.tlaster.precompose.navigation.NavHost
+import moe.tlaster.precompose.navigation.rememberNavigator
+import moe.tlaster.precompose.navigation.transition.NavTransition
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import ui.extension.FadeAnimation
 import ui.screen.emojis.EmojiListScreen
 import ui.screen.onboarding.Onboarding1
 import ui.screen.onboarding.Onboarding2
@@ -17,41 +20,46 @@ import ui.theme.MyAppTheme
 @Composable
 @Preview
 fun App() {
-    MyAppTheme {
-        val contentState = remember { mutableStateOf(0) }
-        AnimatedContent(
-            targetState = contentState,
-            transitionSpec = {
-                fadeIn().plus(slideInVertically(initialOffsetY = { it * 1 /* BOTTOM to UP*/})).togetherWith(fadeOut())
-            },
-        ) {
-            when(it.value) {
-                0 -> {
-                    Onboarding1(
-                        onClick = {
-                            contentState.value = 1
-                        }
-                    )
-                }
-                1 -> {
-                    FadeAnimation(
-                        visible = true
-                    ) {
-                        Onboarding2(
-                            onClick = {
-                                contentState.value = 2
-                            }
-                        )
-                    }
-                }
-                2 -> {
+    PreComposeApp {
+        MyAppTheme {
+            val navigator = rememberNavigator()
+            NavHost(
+                navigator = navigator,
+                navTransition = NavTransition(),
+                initialRoute = "/onboarding1",
+            ) {
+                scene(
+                    route = "/home",
+                    navTransition = NavTransition(),
+                ) {
                     EmojiListScreen {
-                        contentState.value = 3
+                        navigator.navigate("/congratulation")
                     }
                 }
-                3 -> {
+                scene(
+                    route = "/congratulation",
+                    navTransition = NavTransition(
+                        createTransition = slideInVertically(initialOffsetY = { it }),
+                        destroyTransition = fadeOut() + slideOutVertically(
+                            animationSpec = tween(durationMillis = 300, delayMillis = 0, easing = EaseInOut),
+                            targetOffsetY = { it }),
+                        pauseTransition = scaleOut(targetScale = 0.9f),
+                        resumeTransition = scaleIn(initialScale = 0.9f),
+                        exitTargetContentZIndex = 1f,
+                    ),
+                ) {
                     CongratulateScreen {
-                        contentState.value = 2
+                        navigator.goBack()
+                    }
+                }
+                scene(route = "/onboarding1") {
+                    Onboarding1 {
+                        navigator.navigate("/onboarding2")
+                    }
+                }
+                scene(route = "/onboarding2", navTransition = NavTransition()) {
+                    Onboarding2 {
+                        navigator.navigate("/home")
                     }
                 }
             }
