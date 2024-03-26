25bf4d533bc087f27d607b93ee5f0c7eb7e8dbc3
anwar <anwar_pasaribu@live.com>
2024-03-26 23:47:13 +0700

    Query emoji by date range

--- database/src/commonMain/kotlin/com/unwur/mabiaho/database/dao/IUserEmojiDao.kt
+++ database/src/commonMain/kotlin/com/unwur/mabiaho/database/dao/IUserEmojiDao.kt
@@ -4,6 +4,8 @@ import com.unwur.mabiaho.database.domain.EmojiModel
 import kotlinx.coroutines.flow.Flow
 
 interface IUserEmojiDao {
-    fun getUserEmojiByDate(date: Long): Flow<EmojiModel?>
+    fun getUserEmojiByDate(date: Long): Flow<List<EmojiModel>>
+    fun getUserEmojiByTimeStampRange(start: Long, end: Long): Flow<List<EmojiModel>>
+    suspend fun getAllEmoji(): Flow<List<EmojiModel>>
     suspend fun insertEmoji(emojiUnicode: String, timestamp: Long)
 }
--- database/src/commonMain/kotlin/com/unwur/mabiaho/database/dao/UserEmojiDao.kt
+++ database/src/commonMain/kotlin/com/unwur/mabiaho/database/dao/UserEmojiDao.kt
@@ -1,20 +1,47 @@
 package com.unwur.mabiaho.database.dao
 
 import app.cash.sqldelight.coroutines.asFlow
-import app.cash.sqldelight.coroutines.mapToOneNotNull
+import app.cash.sqldelight.coroutines.mapToList
 import com.unwur.mabiaho.database.MabiaDatabase
 import com.unwur.mabiaho.database.domain.EmojiModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class UserEmojiDao(private val database: MabiaDatabase): IUserEmojiDao {
-    override fun getUserEmojiByDate(date: Long): Flow<EmojiModel?> {
-        return database.emojiQueries.selectEmojiByDate(
-            date = date
-        ).asFlow().mapToOneNotNull(Dispatchers.Default).map {
-            EmojiModel(it.id, it.emojiString, it.date)
-        }
+
+    override fun getUserEmojiByDate(date: Long): Flow<List<EmojiModel>> {
+        return database.emojiQueries.selectEmojiByDate(date = date)
+            .asFlow()
+            .mapToList(Dispatchers.Default)
+            .map { list ->
+                list.map {
+                    EmojiModel(it.id, it.emojiString, it.date)
+                }
+            }
+    }
+
+    override fun getUserEmojiByTimeStampRange(start: Long, end: Long): Flow<List<EmojiModel>> {
+        return database.emojiQueries.selectEmojiByTimeStampRange(start, end)
+            .asFlow()
+            .mapToList(Dispatchers.IO)
+            .map { list ->
+                list.map {
+                    EmojiModel(it.id, it.emojiString, it.date)
+                }
+            }
+    }
+
+    override suspend fun getAllEmoji(): Flow<List<EmojiModel>> {
+        return database.emojiQueries.selectAllEmoji()
+            .asFlow()
+            .mapToList(Dispatchers.IO)
+            .map { list ->
+                list.map {
+                    EmojiModel(it.id, it.emojiString, it.date)
+                }
+            }
     }
 
     override suspend fun insertEmoji(emojiUnicode: String, timestamp: Long) {
