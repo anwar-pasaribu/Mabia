d8da85c1e2cf0e0e5ffab6ff186ce3a19803508a
anwar <anwar_pasaribu@live.com>
2024-03-30 18:26:36 +0700

    Animated splash screen for Android

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,6 +58,7 @@ kotlin {
         androidMain.dependencies {
             implementation(libs.compose.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.core.splashscreen)
         }
         iosMain.dependencies {
         }
