738f5d5238eb065f339f15f1698db95f2634d2bf
anwar <anwar_pasaribu@live.com>
2024-04-04 12:29:47 +0700

    Database name as param

--- database/src/desktopMain/kotlin/com/unwur/mabiaho/database/DriverFactory.desktop.kt
+++ database/src/desktopMain/kotlin/com/unwur/mabiaho/database/DriverFactory.desktop.kt
@@ -5,8 +5,8 @@ import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import java.io.File
 
 actual object DriverFactory {
-    actual fun createDriver(): SqlDriver {
-        val databasePath = File(System.getProperty("java.io.tmpdir"), "mabia.db")
+    actual fun createDriver(dbName: String): SqlDriver {
+        val databasePath = File(System.getProperty("java.io.tmpdir"), dbName)
         val driver: SqlDriver = JdbcSqliteDriver(url = "jdbc:sqlite:${databasePath.absolutePath}")
 //        val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
         MabiaDatabase.Schema.create(driver)
