9a21970032c892fe202a9ac48a9f19f4c2846824
Anwar Pasaribu <anwar_pasaribu@live.com>
2024-04-06 20:35:19 +0700

    Bump version 1.0.3 (3)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -133,8 +133,8 @@ android {
         applicationId = "com.unwur.mabiaho"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
     }
     packaging {
         resources {
