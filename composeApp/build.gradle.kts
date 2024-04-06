12d44eaab42b713a8f277f851e90748fd62f9621
Anwar Pasaribu <anwar_pasaribu@live.com>
2024-04-06 17:24:13 +0700

    Fastlane Android: Released new build 3 to Google PlayStore Production track [ci skip]

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -133,7 +133,7 @@ android {
         applicationId = "com.unwur.mabiaho"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0"
     }
     packaging {
