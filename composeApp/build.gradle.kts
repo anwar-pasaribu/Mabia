4eb1c7167eb9cec17fb37e97c4baaa537b631ad5
Anwar Pasaribu <anwar_pasaribu@live.com>
2024-05-06 06:48:20 +0000

    Bumped version to 1.0.13 (build 13) on 06 May 2024, 06:48am

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -135,8 +135,8 @@ android {
         applicationId = "com.unwur.mabiaho"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
-        versionName = "1.0.12"
+        versionCode = 13
+        versionName = "1.0.13"
     }
     packaging {
         resources {
